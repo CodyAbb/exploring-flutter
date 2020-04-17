ad4265bb1555ea288f9a598c939dc07cbfbcde3f
Cody Abbott <cody.abbott16@gmail.com>
2020-04-17 12:07:04 +0100

    initial setup for exploring flutter

---
+++ android/app/src/main/kotlin/com/example/company_name_suggester/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.company_name_suggester
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
